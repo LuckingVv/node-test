c1c357f38b3531c646b8094cbe9a6224813b9f40
wanganwei <william_waw@163.com>
2022-09-02 18:25:32 +0800

    vue代码提交

---
+++ vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">{{ item.q }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    const url = 'http://localhost:8888/search';
+    const method = 'get';
+    axios({
+      url,
+      method,
+      data: { plant: 'CN30', partNumber: '114-0038H' },
+      headers: { 'content-type': 'text/plain; charset=UTF-8' },
+    })
+      .then((res) => {
+        this.list = res.data.g;
+        console.log(res.data.g);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
